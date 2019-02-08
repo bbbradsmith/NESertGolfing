049498a03382d7ffbb1ce33f01ce2ad4c715f1d7
Brad Smith <bbbradsmith@users.noreply.github.com>
2019-02-08 14:09:01 -0500

    avoid deleting runtime.lib if libsrc isn't there to build it with, give diagnostic message

--- build_runtime.bat
+++ build_runtime.bat
@@ -1,7 +1,10 @@
 @REM Generates temp\runtime.lib
 @REM
 @REM cc65\ should contain latest cc65 build (cc65\bin)
-@REM libsrc\ should contain latest cc65 libsrc folder
+@REM librsc\ should contain latest cc65 libsrc folder
+@REM
+@REM Download CC65 source here:
+@REM https://github.com/cc65/cc65
 @REM
 @REM contents:
 @REM   libsrc\runtime
@@ -11,6 +14,7 @@
 @REM     -stkchk.s (not needed, uses .constructor)
 @REM   libsrc\common\copydata.s (used by crt0.s for copying DATA segment to RAM)
 @REM
+if not exist libsrc goto needlibsrc
 md temp\runtime
 del temp\runtime\*.o
 del temp\runtime.lib
@@ -21,4 +25,18 @@ del temp\runtime\callirq.o
 del temp\runtime\callmain.o
 del temp\runtime\stkchk.o
 for %%X in (temp\runtime\*.o) do cc65\bin\ar65.exe a temp\runtime.lib %%X
-pause
+@echo.
+@echo.
+@echo temp/runtime.lib build complete, review the log above in case of errors.
+@goto end
+
+:needlibsrc
+@echo.
+@echo.
+@echo temp/runtime.lib not rebuilt!
+@echo Download CC65 source and place libsrc/ folder here.
+@echo Edit this batch file for more information.
+
+:end
+@echo.
+@pause
